0e9969400f4c2154afa112f3bebd9b36ee1d0c44
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-07-18 17:13:31 -0600

    Added templates

--- src/grocery/views.py
+++ src/grocery/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Price Pal</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
 
-def search_results_view(*args, **kwargs):
-    return HttpResponse("<h1>Search Results</h1>")
+def search_results_view(request, *args, **kwargs):
+    return render(request, "search_results.html", {})
 
-def add_item_view(*args, **kwargs):
-    return HttpResponse("<h1>Add an Item</h1>")
+def add_item_view(request, *args, **kwargs):
+    return render(request, "add_item.html", {})
 
-def shopping_list_view(*args, **kwargs):
-    return HttpResponse("<h1>Shopping List</h1>")
+def shopping_list_view(request, *args, **kwargs):
+    return render(request, "shopping_list.html", {})
