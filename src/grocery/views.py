54805b408a5f1e66399de104d9da743123214dd9
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-07-18 19:26:15 -0600

    Added to search results view

--- src/grocery/views.py
+++ src/grocery/views.py
@@ -1,16 +1,25 @@
 import django
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    return render(request, "grocery/home.html", {})
 
 def search_results_view(request, *args, **kwargs):
-    return render(request, "search_results.html", {})
+    item = Item.objects.get(id=1)
+    context = {
+        "item": item
+    }
+    return render(request, "grocery/search_results.html", context)
 
 def add_item_view(request, *args, **kwargs):
-    return render(request, "add_item.html", {})
+    return render(request, "grocery/add_item.html", {})
 
 def shopping_list_view(request, *args, **kwargs):
-    return render(request, "shopping_list.html", {})
+    context = {
+        "walmart_list": [],
+        "broulims_list": ["Lays Potato Chips", "Salsa"],
+        "albertsons_list": ["Ham", "Doritos", "Milk", "Eggs"],
+    }
+    return render(request, "grocery/shopping_list.html", context)
